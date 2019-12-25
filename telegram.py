e652b0090fca9b9f8463e3a15869b50a029c5b84
Vasilisa <31583978+Ebanchous@users.noreply.github.com>
2019-12-25 08:41:42 +0300

    Add files via upload

---
+++ telegram.py
@@ -0,0 +1,59 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
+# res = requests.get('https://free-proxy-list.net/', headers={'User-Agent':'Mozilla/5.0'})
+# soup = BeautifulSoup(res.text,"lxml")
+# for items in soup.select("#proxylisttable tbody tr"):
+# 	proxy_list = ':'.join([item.text for item in items.select("td")[:2]])
+# 	temp = ('https://' + str(proxy_list))
+# 	proxies={
+# 	'https': temp
+# 	}
+# 	proxies_list.append(proxies)
+
+
+#token = 'AAG3V-oUt_KvvoKvn56yIAu2AmHn-qzYFy0'
+token = '680359542:AAG3V-oUt_KvvoKvn56yIAu2AmHn-qzYFy0'
+
+URL = 'https://api.telegram.org/bot' + token + '/'
+
+proxies = {
+	
+   
+	'https': 'https://27.147.136.178:47678',
+	'http': 'http://183.88.17.55:8080'
+	
+}
+
+def get_updates():
+	url = URL + 'getupdates'
+	r = requests.get(url, proxies=proxies)
+	print('result:', r)
+	return r.json()
+
+def get_message():
+	data = get_updates()
+	chat_id = data['result'][-1]['message']#['chat']['id']
+	message_text = data['result'][-1]['message']#['chat']['text']
+	message = { 
+				'chat_id': chat_id,
+				'text': message_text
+	}
+	print('chat_id:', chat_id)
+	print('message_text:', message_text)
+	return message
+
+def send_message(chat_id, text = 'wait a second please...'):
+	url = URL + 'sendmessage?chat_id={}&text={}'.format(chat_id, text)
+
+
+def main():
+	send_message(13)
+	get_message()
+
+if __name__ == '__main__':
+	main()
+
+
